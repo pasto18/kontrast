e5ce75a6542f44c5a1eff9f71471f74a98bed5e5
pasto18 <94849155+pasto18@users.noreply.github.com>
2026-01-21 00:57:05 +0100

    Instalar adaptador Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,19 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import keystatic from '@keystatic/astro';
 import mdx from '@astrojs/mdx';
+import vercel from '@astrojs/vercel'; // <--- 1. Importamos el adaptador
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
+  // 2. Cambiamos a 'hybrid' (Híbrido: Web estática + Panel de admin dinámico)
+  output: 'hybrid', 
   
+  // 3. Añadimos el adaptador de Vercel
+  adapter: vercel(),
+
   integrations: [
     mdx(),
     react(), 
     keystatic()
   ]
-  
-  // HE BORRADO EL BLOQUE "vite: { ... }" ENTERO.
-  // Vamos a probar si funciona sin él tras la limpieza que hiciste.
 });
